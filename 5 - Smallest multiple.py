13c59edcd60c40a8bd87c761398f041ea144c440
sunwukong123 <33136030+sunwukong123@users.noreply.github.com>
2017-10-26 23:49:28 +0300

    Add files via upload

---
+++ 5 - Smallest multiple.py
@@ -0,0 +1,25 @@
+def primes(x):
+    i = 2
+    result = list()
+   
+    while x % i != x:
+        if x % i == 0:
+            result.append(i)
+            x //= i
+        else:
+            i += 1
+           
+    return result
+
+num = 20
+result = 1
+
+for i in range (1, num + 1 ):
+    temp = result
+    for j in primes(i):
+        if temp % j != 0:
+            result *= j
+        else:
+            temp //= j
+           
+print (result)
