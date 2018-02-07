4e098a707050f0124236acdf43e495283eb12b88
sunwukong123 <33136030+sunwukong123@users.noreply.github.com>
2018-02-07 09:53:08 +0300

    Add files via upload

---
+++ 21 - Amicable numbers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+
+"""
+
+N = 10000
+
+def properDivisorsSum(n):
+    s = 0
+    for i in range(1, n):
+        if n % i == 0:
+            s += i    
+    return s
+    
+l = list(filter(lambda i: properDivisorsSum(properDivisorsSum(i)) == i and properDivisorsSum(i) != i, range(N + 1)))
+print(sum(l))
+
+        
