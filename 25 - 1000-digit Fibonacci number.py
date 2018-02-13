cc3711627824d59916af6701ac298f0a208752f9
sunwukong123 <33136030+sunwukong123@users.noreply.github.com>
2018-02-13 15:59:48 +0300

    Add files via upload

---
+++ 25 - 1000-digit Fibonacci number.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+
+Hence the first 12 terms will be:
+
+    F1 = 1
+    F2 = 1
+    F3 = 2
+    F4 = 3
+    F5 = 5
+    F6 = 8
+    F7 = 13
+    F8 = 21
+    F9 = 34
+    F10 = 55
+    F11 = 89
+    F12 = 144
+
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+"""
+
+from itertools import count
+
+fibs = {1:1, 2:1}
+
+def getFib(num):
+    if num in fibs:
+        return fibs[num]
+    else:
+        fib = getFib(num - 1) + getFib(num - 2)
+        fibs[num] = fib
+        return fib
+
+for i in count(1):
+    if len(str(getFib(i))) == 1000:
+        print(i)
+        break
